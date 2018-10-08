c820d5f7883c774f11a72a1539605e795d5a89e8
carl <Carl@192.168.1.11>
2018-10-07 22:07:09 -0300

    añadida multiplicacion

--- src/main/java/com/mycompany/calculadora/Calculadora.java
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -20,4 +20,8 @@ public class Calculadora {
     public double resta(double num1,double num2){
         return num1-num2;
     }
+     public double multiplicacion(double num1,double num2){
+        return num1*num2;
+    }
+    
 }
